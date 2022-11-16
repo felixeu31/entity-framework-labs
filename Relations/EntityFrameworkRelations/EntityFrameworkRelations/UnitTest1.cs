5c2778654b019839c58a6e8e3f68027ddeebde65
Félix Díez Arias <fdiez@tunstall.es>
2022-11-16 16:52:13 +0100

    test AddingABookToDifferentAuthor_WhenBookIdAlreadyExisting_ShouldThrowDbUpdateException

--- Relations/EntityFrameworkRelations/EntityFrameworkRelations/UnitTest1.cs
+++ Relations/EntityFrameworkRelations/EntityFrameworkRelations/UnitTest1.cs
@@ -133,5 +133,47 @@ namespace EntityFrameworkRelations
                 // The statement has been terminated.
             }
         }
+
+        [Fact]
+        public void AddingABookToDifferentAuthor_WhenBookIdAlreadyExisting_ShouldThrowDbUpdateException()
+        {
+            var bookId = Guid.NewGuid();
+            var bookName = $"BookName_{bookId.ToString()}";
+            var authorId = Guid.NewGuid();
+            var authorName = $"AuthorName_{authorId.ToString()}";
+
+            using (var db = new BookStoreDbContext())
+            {
+                var book = new Book { Name = bookName, BookId = bookId };
+                var author = new Author { Name = authorName, AuthorId = authorId };
+                author.Books.Add(book);
+                db.Authors.Add(author);
+
+                db.SaveChanges();
+            }
+
+            using (var db = new BookStoreDbContext())
+            {
+                var author2Id = Guid.NewGuid();
+                var author2Name = $"Author2Name_{authorId.ToString()}";
+                var author2 = new Author { Name = author2Name, AuthorId = author2Id };
+                
+                var repeatedBook = new Book { Name = $"{bookName}_Repeated", BookId = bookId };
+                author2.Books.Add(repeatedBook);
+                
+                db.Authors.Add(author2);
+
+                Action action = () => db.SaveChanges();
+
+                var ex = Assert.Throws<Microsoft.EntityFrameworkCore.DbUpdateException>(action);
+
+                //ex.Message
+                //An error occurred while saving the entity changes. See the inner exception for details.
+
+                // ex.InnerException.Message
+                // Violation of PRIMARY KEY constraint 'PK_Books'.Cannot insert duplicate key in object 'dbo.Books'.The duplicate key value is (5f6888bf - 97e2 - 49b6 - 9eb3 - 4de84429f2d0).
+                // The statement has been terminated.
+            }
+        }
     }
 }
